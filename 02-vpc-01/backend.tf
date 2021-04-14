403821285e1027329614d4f7ff6f437d4c6e6218
Veikko Kummich <veikko.kummich@tf.de>
2021-04-14 12:53:16 +0200

    vpc use module

--- 02-vpc-01/backend.tf
+++ 02-vpc-01/backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket         = "terraform-training-s3-backend-team-muc"
     encrypt        = true
-    key            = "02-vpc-01"
+    key            = "03-secgroup"
     region         = "eu-central-1"
     dynamodb_table = "terraform-lock-muc"
     profile        = "terraform-training"
