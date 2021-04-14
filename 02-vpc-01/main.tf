d34e795750df7a99e73b7e4ce52583c30989c63a
Veikko Kummich <veikko.kummich@tf.de>
2021-04-14 13:37:01 +0200

    finish

--- 02-vpc-01/main.tf
+++ 02-vpc-01/main.tf
@@ -1,14 +1,13 @@
 module "vpc" {
   source = "../modules/terraform-module-aws-vpc"
 
-
-  availability_zones  = var.availability_zones
-  cidr_block          = var.cidr_block[terraform.workspace]
+  availability_zones  = [ "eu-central-1a" ]
+  cidr_block          = "10.10.0.0/16"
   domain_name         = "cloud.local"
-  domain_name_servers = ["8.8.8.8", "1.1.1.1"]
+  domain_name_servers = [ "8.8.8.8", "1.1.1.1" ]
   name                = "vpc-${terraform.workspace}"
-  namespace           = var.namespace
-  private_subnets     = var.private_subnets[terraform.workspace]
-  public_subnets      = var.public_subnets[terraform.workspace]
+  namespace           = "svm"
+  private_subnets     = [ "10.10.1.0/24" ]
+  public_subnets      = [ "10.10.2.0/24" ]
   stage               = terraform.workspace
 }
